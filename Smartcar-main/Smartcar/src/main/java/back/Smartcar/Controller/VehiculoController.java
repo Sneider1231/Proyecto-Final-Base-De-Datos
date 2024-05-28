4dfeb78c4e0f51f45869244e6a83cb11d84f6d41
stephanvargas <smonroy-2022a@corhuila.edu.co>
2024-05-27 23:24:25 -0500

    Subimos el proyecto
    Subimos el proyecto

---
+++ Smartcar-main/Smartcar/src/main/java/back/Smartcar/Controller/VehiculoController.java
@@ -0,0 +1,42 @@
+package back.Smartcar.Controller;
+
+import back.Smartcar.Entity.Vehiculo;
+import back.Smartcar.IService.IVehiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("vehiculo")
+public class VehiculoController {
+    @Autowired
+    private IVehiculoService service;
+
+    @GetMapping()
+    public List<Vehiculo> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Vehiculo> findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @PostMapping()
+    public Vehiculo save(@RequestBody Vehiculo vehiculo){
+        return service.save(vehiculo);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody Vehiculo vehiculo, @PathVariable Long id){
+        service.update(vehiculo, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+}
--- back/src/main/java/com/example/back/Repositorio/VehiculoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.back.Repositorio;
-
-import com.example.back.Modelo.Vehiculo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiculoRepositorio extends JpaRepository<Vehiculo, Integer> {
-}
